afbe1f940ee73ec48d01b4622713a00cd297a0e3
home-strm <78903768+home-strm@users.noreply.github.com>
2021-11-18 22:36:36 +0000

    v1
    first version of codename aza

---
+++ initial/accountreg.bat
@@ -0,0 +1,72 @@
+@echo off
+color 0a
+cls
+goto accountreg1
+
+:: introductory thingy majig
+:accountreg1
+cls
+echo Welcome to Codename AZA!
+timeout /t 3 /nobreak >nul
+cls
+echo As this is your first time using Codename AZA, you must make an account before you continue
+timeout /t 6 /nobreak >nul
+cls
+goto usernamecreate
+
+:: username entry
+:usernamecreate
+cls
+echo Lets get started!
+echo Please enter your desired username.
+set /p input=
+goto accountreg2
+
+:: double checking the username
+:accountreg2
+cls
+echo %input%, is this the username you desire? If not, please type 'no' to go back and 'yes' to continue.
+set /p doublecheck=
+if %doublecheck% == yes goto accountreg3
+if %doublecheck% == no goto usernamecreate
+
+:: pasword entry
+:accountreg3
+cls
+echo Great choice! Now lets move on to the password.
+echo Please enter your desired password.
+set /p pass1=
+goto accountreg4
+
+:: double checking the password
+:accountreg4
+cls
+echo %pass1%, are you sure you want this to be your password? If not, please type 'no' to go back and 'yes' to continue.
+set /p doublecheck2=
+if %doublecheck2% == yes goto accountreg5
+if %doublecheck2% == no goto accountreg3
+
+:: alot of stuff like making some important folders and making the user folder and writing the loginconfig
+:accountreg5
+cls
+echo Great! The setup is almost completed, please give us a moment to finish up.
+cd ..
+mkdir Users
+mkdir sysfiles
+mkdir preinstalled
+cd config
+echo .>"loginconfig.txt"
+echo %input%> loginconfig.txt
+echo %pass1%>> loginconfig.txt
+echo 0a>"colorconf.txt"
+cd ..
+cd Users
+mkdir %input%
+cd ..
+timeout /t 10 /nobreak >nul
+cls
+echo Great! The setup is now finished. Enjoy your fresh install of Codename AZA.
+:: nice
+echo Rebooting to Codename AZA in 5 seconds.
+timeout /t 5 /nobreak >nul
+call start
---
+++ start.bat
@@ -0,0 +1,69 @@
+@echo off
+color 0a
+cls
+goto verify
+
+:: checks for the user login.
+:verify
+cd config
+if exist loginconfig.txt goto verify2
+if not exist loginconfig.txt goto accountreg
+
+:accountreg
+cd ..
+cd initial
+call accountreg.bat
+
+:: boot sequence
+:verify2
+cd ..
+echo Welcome back to Codename AZA!
+echo Please wait for the system to finish booting..
+timeout /t 2 /nobreak >nul
+echo MEMCHECK.. GOOD!
+timeout /t 1 /nobreak >nul
+echo SYSTEMFILECHECK.. PASS!
+timeout /t 2 /nobreak >nul
+goto login1
+
+:: username check sequence
+:login1
+cls
+cd config
+<loginconfig.txt (
+set /p usercheck=
+set /p passcheck=
+)
+echo Wellcome back to codename AZA! Please enter your username.
+set /p user=Username: 
+if %user% == %usercheck% goto login2
+else goto userrecheck
+
+:: incorrect username sequence
+:userrecheck
+cls
+echo Username incorrect!
+echo If you would like to try again please type 1, if you would like to reset your username type 2.
+set /p recheck=
+if %recheck% == 1 goto login1
+if %recheck% == 2 goto usernameredo
+
+:: second part of the login, password check sequence
+:login2
+cls
+set /p pass=Password: 
+if %pass% == %passcheck% goto gui
+else goto incorrectpass
+
+:: incorrect password sequence
+:incorrectpass
+cls
+echo Password incorrect!
+echo If you would like to try again please type 1, if you would like to reset your password type 2.
+set /p rechec2k=
+if %recheck2% == 1 goto login2
+if %recheck2% == 2 goto passwordredo
+
+:gui
+echo eyyy nice
+pause
